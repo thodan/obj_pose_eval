780227647b2f9b312c4f1796daa4f52d619b7725
Tomas Hodan <tom.hodan@gmail.com>
2016-11-09 15:20:02 +0100

    Fixed setup script.

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from distutils.core import setup
 setup(
     name='obj_pose_eval',
     version='1.0',
-    py_modules=['obj_pose_eval'],
-    license='The MIT License',
+    description='Tools for evaluation of 6D object pose estimation',
     long_description=open('README.md').read(),
+    author='Tomas Hodan',
+    author_email='hodantom@cmp.felk.cvut.cz',
+    url='https://github.com/thodan/obj_pose_eval'
+    packages=['obj_pose_eval'],
+    license='The MIT License',
     install_requires=['scipy', 'numpy', 'glumpy', 'matplotlib']
 )
